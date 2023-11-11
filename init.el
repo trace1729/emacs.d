62fa0fcd28651617b900f4f0c85c556c00464383
20231011-Gong Kai Chen <1106029170@qq.com>
2023-11-11 23:16:44 +0800

    update

--- init.el
+++ init.el
@@ -10,9 +10,9 @@
 
 ;----------------- keyboard rebinding-------------------------;
 (global-set-key (kbd "s-a") 'mark-whole-buffer) ;;对应Windows上面的Ctrl-a 全选
-(global-set-key (kbd "s-k") 'kill-ring-save) ;;对应Windows上面的Ctrl-c 复制
+(global-set-key (kbd "C-c y") 'kill-ring-save) ;;对应Windows上面的Ctrl-c 复制
 (global-set-key (kbd "s-s") 'save-buffer) ;; 对应Windows上面的Ctrl-s 保存
-(global-set-key (kbd "s-v") 'yank) ;对应Windows上面的Ctrl-v 粘贴
+(global-set-key (kbd "C-c p") 'yank) ;对应Windows上面的Ctrl-v 粘贴
 (global-set-key (kbd "s-z") 'undo) ;对应Windows上面的Ctrol-z 撤销
 (global-set-key (kbd "s-x") 'kill-region) ;对应Windows上面的Ctrol-x 剪切
 (global-set-key (kbd "C-c s") 'eshell); open a shell
@@ -39,6 +39,25 @@
 ;; 这一行代码，将函数 open-init-file 绑定到  键上
 (global-set-key (kbd "C-c C-t") 'open-org)
 
+
+;================ user defined function =====================;
+
+(defun consult-directory-externally (file)
+  "Open FILE externally using the default application of the system."
+  (interactive "fOpen externally: ")
+  (call-process (pcase system-type
+		  ('gnu/linux "nautilus"))
+		nil 0 nil
+		(file-name-directory
+		 (expand-file-name file))))
+
+; 打开当前文件的目录
+(defun my-open-current-directory ()
+  (interactive)
+  (consult-directory-externally default-directory))
+
+;(define-key embark-file-map (kbd "E") #'consult-directory-externally)
+
 ;---------------- custmisze editor apperance ----------------;
 ;; close tool-bar
 (tool-bar-mode -1)
@@ -47,7 +66,10 @@
 (scroll-bar-mode -1)
 
 ;; 显示行号
-(global-display-line-numbers-mode)
+;(global-display-line-numbers-mode)
+(add-hook 'prog-mode-hook (lambda() (display-line-numbers-mode 1)))
+(add-hook 'text-mode-hook (lambda() (display-line-numbers-mode 1)))
+(add-hook 'pdf-view-mode (lambda() (display-line-numbers-mode nil)))
 
 ;; change font size
 (set-face-attribute 'default nil :height 160)
@@ -95,7 +117,7 @@
  '(custom-safe-themes
    '("8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "f4d1b183465f2d29b7a2e9dbe87ccc20598e79738e5d29fc52ec8fb8c576fcfd" "c8b3d9364302b16318e0f231981e94cbe4806cb5cde5732c3e5c3e05e1472434" "be84a2e5c70f991051d4aaf0f049fa11c172e5d784727e0b525565bb1533ec78" "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" default))
  '(package-selected-packages
-   '(doom-themes embark-consult consult embark marginalia gnu-elpa-keyring-update vertico uimage org-download grip-mode keycast htmlize rainbow-mode racket-mode org markdown-mode evil company)))
+   '(fanyi org-noter pdf-tools doom-themes embark-consult consult embark marginalia gnu-elpa-keyring-update vertico uimage org-download grip-mode keycast htmlize rainbow-mode racket-mode org markdown-mode evil company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -168,3 +190,16 @@
 (global-set-key (kbd "C-s") 'consult-line)
 ;;consult-imenu
 (global-set-key (kbd "C-c i") 'consult-imenu)
+
+;; org-noter
+(setq org-noter-notes-search-path '("/home/trace/Documents/org/pdf_notes")) ;; 默认笔记路径
+(setq org-noter-auto-save-last-location t) ;; 自动保存上次阅读位置
+(setq org-noter-max-short-selected-text-length 20) ;; 默认为 80
+(setq org-noter-default-heading-title "第 $p$ 页的笔记") ;; 默认短标题格式
+(global-set-key (kbd "C-c n n") 'org-noter) ;; 与 org-roam 配合
+
+
+;; pdf-tools
+(pdf-loader-install)
+(add-hook 'pdf-view-mode-hook 'pdf-view-fit-width-to-window) ;; 自动放大到页宽
+
